9b59684b9f5241883b097119d6c26b8cd36d7619
minnie-star <minenhleminnie8@gmail.com>
2025-05-06 19:48:56 +0200

    exercise 2 grade letter

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -8,7 +8,7 @@ class Program
         Console.WriteLine("What is your name?" );
         string name = Console.ReadLine();
         Console.WriteLine("What is your last name? ");
-        string last_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {last_name}, {name} {last_name}.");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
     }
 }
