f30f69df88bbe119f78bd95420dbd16f43991ad8
minnie-star <minenhleminnie8@gmail.com>
2025-05-07 16:17:05 +0200

    exercise 3 guess number game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int numberToGuess = random.Next(1, 100); 
+        int userGuess = 0;
+
+        Console.WriteLine("Guess a number between 1 and 100:");
+
+        while (userGuess != numberToGuess)
+        {
+            Console.Write("What is your guess: ");
+            
+            
+            if (int.TryParse(Console.ReadLine(), out userGuess))
+            {
+                if (userGuess < numberToGuess)
+                {
+                    Console.WriteLine("Too low, try again!");
+                }
+                else if (userGuess > numberToGuess)
+                {
+                    Console.WriteLine("Too high, try again!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
+
+        Console.WriteLine($"Correct! The number was {numberToGuess}.");
+    
+        
+        }
     }
-}
+
+
+
+
+        
