9010250a5ed8cec75659eb0e18ffd3c26371b07f
minnie-star <minenhleminnie8@gmail.com>
2025-05-07 20:32:40 +0200

    exercise 4 list of numbers

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished: ");
+        int number;
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0); 
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"The average is {average}");
+
+        double maximunm = numbers.Max();
+        Console.WriteLine($"The maximum number is: {maximunm}");
     }
 }
