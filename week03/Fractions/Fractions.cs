325be3cebd3bf6b1b3fdf8e1f28c4fba6043b684
minnie-star <minenhleminnie8@gmail.com>
2025-05-17 16:14:57 +0200

    week 3 fractions exercise

--- week03/Fractions/Fractions.cs
+++ week03/Fractions/Fractions.cs
@@ -1,4 +1,36 @@
 public class Fractions
 {
-    
+    private int _topNumber;
+    private int _bottomNumber;
+
+    public Fractions()
+    {
+        _topNumber = 1;
+        _bottomNumber = 1;
+    }
+
+    public Fractions(int wholeNumber)
+    {
+        _topNumber = wholeNumber;
+        _bottomNumber = 1;
+    }
+
+    public Fractions(int top, int bottom)
+    {
+        _topNumber = top;
+        _bottomNumber = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_topNumber}/{_bottomNumber}";
+        return text;
+    }
+
+    public double GetDecimalValue() 
+    {
+        return (double)_topNumber / (double)_bottomNumber;
+    }
+
+
 }
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,8 +1,20 @@
 using System;
-
 class Program
+{
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fractions f1 = new Fractions();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fractions f2 = new Fractions(5, 3);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fractions f3 = new Fractions(3, 4);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
     }
 }
