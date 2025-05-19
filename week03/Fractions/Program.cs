24fbcef6a561d7db515c9f381f77f411c68c2eeb
minnie-star <minenhleminnie8@gmail.com>
2025-05-19 12:29:12 +0200

    fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -10,11 +10,19 @@ class Program
         Console.WriteLine(f1.GetDecimalValue());
 
         Fractions f2 = new Fractions(5, 3);
-        Console.WriteLine(f1.GetFractionString());
-        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
 
         Fractions f3 = new Fractions(3, 4);
-        Console.WriteLine(f1.GetFractionString());
-        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fractions f4 = new Fractions(1, 4);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        Fractions f5 = new Fractions(1, 2);
+        Console.WriteLine(f5.GetFractionString());
+        Console.WriteLine(f5.GetDecimalValue());
     }
 }
